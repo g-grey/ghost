aedf5a16da5006b9a794e58d2112cb32bd1efb4a
iheemskerk <ivan.heemskerk@academicapproach.com>
2018-04-23 15:59:50 -0400

    bash script that displays animated ascii ghost

---
+++ ghost.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+FRAMES[0]=$(cat << END
+    _____
+   /      \\
+   |'   ' |
+   |      |
+    \/\/\/
+END
+)
+
+FRAMES[1]=$(cat << END
+    _____
+   /      \\
+   |.   . |
+   |      |
+   |/\/\/\|
+END
+)
+
+trap bye INT
+
+bye() {
+    printf "\ngooOOodbye..."
+    exit 0
+}
+
+printf "\033[0;31m"
+
+while true
+do
+    for FRAME in "${FRAMES[@]}"
+    do
+        clear
+        printf "$FRAME\n"
+        sleep .3s
+    done
+done
